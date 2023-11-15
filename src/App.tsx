6f447df42025c57e55441951c6952251345bd8ca
George <tsintsadze7@gmail.com>
2023-11-15 22:40:59 +0400

    init: empty project

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,8 @@
 import './App.css'
-import { useState } from 'react'
-import ItemListForm from './components/ItemListForm';
-import ItemListComponent from './components/ItemListComponent';
-
-export type ItemListType = {
-  id: string;
-  value: string;
-}
 
 export default function App() {
-  const [textIputValue, setTextIputValue] = useState("...")
-  const [itemList, setItemList] = useState<ItemListType[]>([])
-
-  function handleSubmit(e: React.FormEvent) {
-    e.preventDefault()
-    if (!textIputValue) return
-    setItemList((currentItems) => {
-      return [...currentItems, { id: crypto.randomUUID(), value: textIputValue }]
-    })
-
-    document.getElementById('input')?.focus()
-  }
-
   return (
     <>
-      <hr />
-      <h1 className='title'>Hello</h1>
-      <hr />
-      <p className="text-input-value">{textIputValue}</p>
-
-      <ItemListForm
-        handleSubmit={handleSubmit}
-        textIputValue={textIputValue}
-        setTextIputValue={setTextIputValue} />
-
-      <ItemListComponent itemList={itemList} />
-      <hr />
     </>
   )
 }
