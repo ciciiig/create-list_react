d69a46319a94f7ecc02d6162ee8bcdde6bfe9f5d
George <tsintsadze7@gmail.com>
2023-11-17 14:26:38 +0400

    feat: useMemo

--- src/components/UseMemo/UseMemo.tsx
+++ src/components/UseMemo/UseMemo.tsx
@@ -1,14 +1,47 @@
-import { FC } from "react";
+import "./UseMemo.style.css"
+import { FC, useEffect, useMemo, useState } from "react";
 import { UseMemoRules } from "../UseMemo/UseMemoRules/UseMemoRules";
 
 export const UseMemo: FC = () => {
+    const [number, setNumber] = useState(0);
+    const [dark, setDark] = useState(false);
+    
+    const doubleNumber = slowFunction(number);
+    // const doubleNumber = useMemo(() => {
+    //     return slowFunction(number);
+    // }, [number]);
+
+    const themeStyles = {
+        backgroundColor: dark ? 'black' : 'white',
+        color: dark ? 'white' : 'black'
+    }
+
+    // // const themeStyles = useMemo(() => {
+    // //     return {
+    // //         backgroundColor: dark ? 'black' : 'white',
+    // //         color: dark ? 'white' : 'black'
+    // //     }
+    // // }, [dark])
+    
+
+    // useEffect(() => {
+    //     console.log('Theme Changed');
+    // }, [themeStyles])
 
     return (
         <div className="container">
             <div className="useMemo-container">
-                
+                <input type="number" value={number} onChange={e => setNumber(parseInt(e.target.value))} />
+                <button onClick={() => setDark(prevDark => !prevDark)}>Change Theme</button>
+                <div style={themeStyles}>{doubleNumber}</div>
             </div>
+
             <UseMemoRules />
         </div>
     )
+
+    function slowFunction(num: number) {
+        for (let i = 0; i < 1000000000; i++) {}
+        return num * 2;
+    }
 }
--- src/components/UseMemo/UseMemoRules/UseMemoRules.tsx
+++ src/components/UseMemo/UseMemoRules/UseMemoRules.tsx
@@ -6,7 +6,13 @@ export const UseMemoRules: FC = () => {
         <div className="useMemo-rules-container">
             <h3>UseMemo Rules</h3>
             <ul>
-                <li></li>
+                <li>it’s caching a value, not to recompute it on every render.</li>
+                <li>In values passed by reference useMemo will work if we choose exact value inside of it. For example, in case of object a value inside of an object should be passed as a changeable value in useMemo.</li>
+                <li>Why not to use it on everything:
+                    <ul>
+                        <li>On each render useMemo function has to be called and value must be saved in memory. </li>
+                    </ul>
+                </li>
             </ul>
         </div>
     )
